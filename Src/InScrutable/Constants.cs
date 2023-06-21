d033becd50a818c124bda0eddf0dc8addcd62762
ASquigglyTwist <asquigglytwist@gmail.com>
2023-06-21 21:53:24 +0530

    Adding supporting elements

---
+++ Src/InScrutable/Constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text;
+
+namespace InScrutable.Global
+{
+    internal static class Constants
+    {
+        internal static ImmutableHashSet<char> KnownVowels = ImmutableHashSet.Create('A', 'E', 'I', 'O', 'U', 'Y');
+
+        internal static bool IsVowelOrY(this char alphabet)
+        {
+            var alphabetInUpperCase = char.ToUpper(alphabet, System.Globalization.CultureInfo.InvariantCulture);
+            return KnownVowels.Any(vowel => vowel == alphabetInUpperCase);
+        }
+    }
+}
