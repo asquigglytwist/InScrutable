26dc648b484c03d2b3afc0e50f2cfaf804d8d15a
ASquigglyTwist <asquigglytwist@gmail.com>
2023-06-22 18:10:52 +0530

    ClusterMarker implementation
    Implementation of struct - ClusterMarker to preserve the indices of substrings of interest

--- Src/InScrutable/Argot.cs
+++ Src/InScrutable/Argot.cs
@@ -4,6 +4,39 @@ using System.Text;
 
 namespace InScrutable
 {
+    internal struct ClusterMarker
+    {
+        private const int UnInitializedIndex = -1;
+
+        internal int ClusterStartIndex { get; private set; }
+
+        internal int ClusterEndIndex { get; private set; }
+
+        internal bool IsInitialized { get; private set; }
+
+        public ClusterMarker()
+        {
+            ResetToInitState();
+        }
+
+        internal ClusterMarker(int clusterStart, int clusterEnd)
+        {
+            Assign(clusterStart, clusterEnd);
+        }
+
+        internal void ResetToInitState()
+        {
+            ClusterStartIndex = ClusterEndIndex = UnInitializedIndex;
+            IsInitialized = false;
+        }
+
+        internal void Assign(int clusterStart, int clusterEnd)
+        {
+            ClusterStartIndex = clusterStart;
+            ClusterEndIndex = clusterEnd;
+            IsInitialized = true;
+        }
+    }
     internal static class Argot
     {
         internal static string Scramble(string plainString)
