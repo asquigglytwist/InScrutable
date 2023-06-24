ab7a7e7d023f81d7702f3b8bacaabd9c1c1c7e14
ASquigglyTwist <asquigglytwist@gmail.com>
2023-06-24 15:53:26 +0530

    Adding Debug capabilities to ClusterMarker
    Adding Debug capabilities to ClusterMarker:
    *  Preserving OriginalCompleteString as a ReadOnly during construction
    *  An independent prop - SubStringCluster for reflecting the current SubString of interest
    *  Modifying said property on every re-assignment

--- Src/InScrutable/Argot.cs
+++ Src/InScrutable/Argot.cs
@@ -16,10 +16,23 @@ namespace InScrutable
 
         internal bool IsInitialized { get; private set; }
 
+#if DEBUG
+        internal readonly string OriginalCompleteString;
+
+        internal string SubStringCluster { get; private set; }
+#endif
+
         public ClusterMarker()
         {
             ResetToInitState();
         }
+
+#if DEBUG
+        internal ClusterMarker(string original)
+        {
+            OriginalCompleteString = original;
+        }
+#endif
 
         internal ClusterMarker(int clusterStart, int clusterEnd)
         {
@@ -30,6 +43,9 @@ namespace InScrutable
         {
             ClusterStartIndex = ClusterEndIndex = UnInitializedIndex;
             IsInitialized = false;
+#if DEBUG
+            SubStringCluster = string.Empty;
+#endif
         }
 
         internal void Assign(int clusterStart, int clusterEnd)
@@ -37,6 +53,13 @@ namespace InScrutable
             ClusterStartIndex = clusterStart;
             ClusterEndIndex = clusterEnd;
             IsInitialized = true;
+#if DEBUG
+            if (!string.IsNullOrEmpty(OriginalCompleteString))
+            {
+                SubStringCluster = OriginalCompleteString.Substring(ClusterStartIndex,
+                    ClusterEndIndex - ClusterStartIndex + 1);
+            }
+#endif
         }
     }
 
