c8cc30de0ad56f6f88707116881c21b52f6ca9d4
ASquigglyTwist <asquigglytwist@gmail.com>
2023-06-24 17:18:54 +0530

    [Ref]  Extracting extension methods
    Minor refactoring - Extracting extension methods to an independent CS file
    *  Unknown change in file

--- Src/InScrutable/Constants.cs
+++ Src/InScrutable/Constants.cs
@@ -8,16 +8,5 @@ namespace InScrutable.Global
     internal static class Constants
     {
         internal static ImmutableHashSet<char> KnownVowels = ImmutableHashSet.Create('A', 'E', 'I', 'O', 'U', 'Y');
-
-        internal static bool IsVowelOrY(this char alphabet)
-        {
-            var alphabetInUpperCase = char.ToUpper(alphabet, System.Globalization.CultureInfo.InvariantCulture);
-            return KnownVowels.Any(vowel => vowel == alphabetInUpperCase);
-        }
-
-        internal static bool IsNotVowelOrY(this char alphabet)
-        {
-            return !alphabet.IsVowelOrY();
-        }
     }
 }
---
+++ Src/InScrutable/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InScrutable
+{
+    internal static class Extensions
+    {
+        internal static bool IsVowelOrY(this char alphabet)
+        {
+            var alphabetInUpperCase = char.ToUpper(alphabet, System.Globalization.CultureInfo.InvariantCulture);
+            return Global.Constants.KnownVowels.Any(vowel => vowel == alphabetInUpperCase);
+        }
+
+        internal static bool IsNotVowelOrY(this char alphabet)
+        {
+            return !alphabet.IsVowelOrY();
+        }
+    }
+}
