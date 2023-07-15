0ac5da1547f169a3e139125636982ed3512ac35a
ASquigglyTwist <asquigglytwist@gmail.com>
2023-07-15 14:44:52 +0530

    [Minor]  Doc updates
    Doc updates to:  Constants

--- Src/InScrutable/Helpers/Constants.cs
+++ Src/InScrutable/Helpers/Constants.cs
@@ -5,8 +5,15 @@ using System.Text;
 
 namespace InScrutable.Global
 {
+    /// <summary>
+    /// <para>All Constant values (that can be hard-coded)</para>
+    /// Typically, all baseline assumptions / immutable values
+    /// </summary>
     internal static class Constants
     {
         internal static ImmutableHashSet<char> KnownVowels = ImmutableHashSet.Create('A', 'E', 'I', 'O', 'U', 'Y');
+        /// <summary>
+        /// List (Set) of vowels from alphabets of (mutiple) language(s)
+        /// </summary>
     }
 }
