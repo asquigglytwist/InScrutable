f4f3818d874af459eab728128533e667749b314d
ASquigglyTwist <asquigglytwist@gmail.com>
2023-07-18 17:02:18 +0530

    Adding support for additional language alphabets
    Adding support for alphabets in additional languages:
    *  Indic - TA, ML, TE, SA
    *  EU - GE, FR

--- Src/InScrutable/Helpers/Constants.cs
+++ Src/InScrutable/Helpers/Constants.cs
@@ -11,9 +11,28 @@ namespace InScrutable.Global
     /// </summary>
     internal static class Constants
     {
-        internal static ImmutableHashSet<char> KnownVowels = ImmutableHashSet.Create('A', 'E', 'I', 'O', 'U', 'Y');
         /// <summary>
         /// List (Set) of vowels from alphabets of (mutiple) language(s)
         /// </summary>
+        internal static ImmutableHashSet<char> KnownVowels = ImmutableHashSet.Create(
+            // EN
+            'A', 'E', 'I', 'O', 'U', 'Y'
+
+            // Indic Langs
+            // TA (Independent)
+            , 'அ', 'ஆ', 'இ', 'ஈ', 'உ', 'ஊ', 'எ', 'ஏ', 'ஐ', 'ஒ', 'ஓ', 'ஔ'
+            // ML
+            , 'അ', 'ആ', 'ഇ', 'ഈ', 'ഉ', 'ഉ', 'ഋ', 'ൠ', 'ഌ', 'ൡ', 'എ', 'ഏ', 'ഒ', 'ഓ', 'ഐ', 'ഔ'
+            // TE
+            , 'అ', 'ఆ', 'ఇ', 'ఈ', 'ఉ', 'ఊ', 'ఋ', 'ఌ', 'ఎ', 'ఏ', 'ఐ', 'ఒ', 'ఓ', 'ఔ'
+            // SA
+            , 'अ', 'आ', 'इ', 'ई', 'उ', 'ऊ', 'ऋ', 'ॠ', 'ऌ', 'ॡ', 'ए', 'ऐ', 'ओ', 'औ'
+
+            // EU Langs
+            // GE
+            , 'ä', 'ö', 'ü'
+            // FR
+            , 'â', 'à', 'è', 'ê', 'û', 'î'
+            );
     }
 }
