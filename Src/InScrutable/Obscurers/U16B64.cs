86ba3c59b3639a22c9cc91aa9e327755b6a8f925
ASquigglyTwist <asquigglytwist@gmail.com>
2023-08-04 12:59:54 +0530

    [Ft] Add Base64 (UTF16)
    Feature - Add support for Base64 (UTF16)

---
+++ Src/InScrutable/Obscurers/U16B64.cs
@@ -0,0 +1,24 @@
+﻿using InScrutable.Global;
+using InScrutable.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace InScrutable.Obscurers
+{
+    internal class U16B64 : IArgot
+    {
+        #region IArgot Implementation
+        string IArgot.Obscure(string plainString)
+        {
+            return Convert.ToBase64String(Encoding.Unicode.GetBytes(plainString));
+        }
+
+        string IArgot.Reveal(string obscuredString)
+        {
+            return Encoding.Unicode.GetString(Convert.FromBase64String(obscuredString));
+        }
+        #endregion
+    }
+}
