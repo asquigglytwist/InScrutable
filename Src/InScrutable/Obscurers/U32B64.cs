ac6b1869449de2d3eeb0e0e21c5958c5ca920e1a
ASquigglyTwist <asquigglytwist@gmail.com>
2023-08-04 13:03:14 +0530

    [Ft] Add Base64 (UTF32)
    Feature - Add support for Base64 (UTF32)

---
+++ Src/InScrutable/Obscurers/U32B64.cs
@@ -0,0 +1,24 @@
+﻿using InScrutable.Global;
+using InScrutable.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace InScrutable.Obscurers
+{
+    internal class U32B64 : IArgot
+    {
+        #region IArgot Implementation
+        string IArgot.Obscure(string plainString)
+        {
+            return Convert.ToBase64String(Encoding.UTF32.GetBytes(plainString));
+        }
+
+        string IArgot.Reveal(string obscuredString)
+        {
+            return Encoding.UTF32.GetString(Convert.FromBase64String(obscuredString));
+        }
+        #endregion
+    }
+}
