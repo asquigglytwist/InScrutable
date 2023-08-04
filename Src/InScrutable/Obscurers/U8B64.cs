2f595dd4421fdf58898bb497c86e357dd7465889
ASquigglyTwist <asquigglytwist@gmail.com>
2023-08-04 12:57:24 +0530

    [Ft]  Add Base64 (UTF8)
    Feature - Add support for Base64 (UTF8)

---
+++ Src/InScrutable/Obscurers/U8B64.cs
@@ -0,0 +1,24 @@
+﻿using InScrutable.Global;
+using InScrutable.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace InScrutable.Obscurers
+{
+    internal class U8B64 : IArgot
+    {
+        #region IArgot Implementation
+        string IArgot.Obscure(string plainString)
+        {
+            return Convert.ToBase64String(Encoding.UTF8.GetBytes(plainString));
+        }
+
+        string IArgot.Reveal(string obscuredString)
+        {
+            return Encoding.UTF8.GetString(Convert.FromBase64String(obscuredString));
+        }
+        #endregion
+    }
+}
